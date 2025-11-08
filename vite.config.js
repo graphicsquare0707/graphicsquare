fb7ece56f0ba4b397efaa64c3a8228ac2eb0a1b2
graphicsquare0707 <hinatanoyume11@gmail.com>
2025-11-09 04:19:44 +0900

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/',       // 公開ルートを明示
+  build: {
+    outDir: 'dist' // ビルド成果物を dist に出力
+  }
 })
+
+
