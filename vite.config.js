699348bfbf149c2596fd92b9f6b0eec8b6702d05
graphicsquare0707 <hinatanoyume11@gmail.com>
2025-11-08 21:15:36 +0900

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  preview: {
+    allowedHosts: ['graphicsquare-g9f0.onrender.com'],
+    host: true,
+    port: 4173
+  }
 })
