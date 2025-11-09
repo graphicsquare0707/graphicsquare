54914fe6f3a77599bbffb65aa12888e6bbac7795
graphicsquare0707 <hinatanoyume11@gmail.com>
2025-11-09 19:24:27 +0900

    Add files via upload

--- vue/vite.config.js
+++ vue/vite.config.js
@@ -1,7 +1,8 @@
+// vue/vite.config.js
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/vue/',
+  base: '/vue/',   // ← ここを変更
 })
